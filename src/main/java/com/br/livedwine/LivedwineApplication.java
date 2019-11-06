eede0fad38e77192cb3761c8aab6c03e5765275d
Lucas Copque <copque.faculdade@gmail.com>
2019-11-06 13:12:52 -0300

    initial project

---
+++ src/main/java/com/br/livedwine/LivedwineApplication.java
@@ -0,0 +1,13 @@
+package com.br.livedwine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivedwineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivedwineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/livedwine/LivedwineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.livedwine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivedwineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
